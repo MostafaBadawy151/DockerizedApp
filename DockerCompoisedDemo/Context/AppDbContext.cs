e81ebb2f6a5b732d0b9543fd4ed1ff1b2eb6564b
Mostafa Ahmed <Mostafa.Ahmed@LinkDev.com>
2025-03-18 16:26:02 +0200

    AddFiles

---
+++ DockerCompoisedDemo/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DockerCompoisedDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DockerCompoisedDemo.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
---
+++ DockerCompoisedDemo/Migrations/20250316152200_ChangeColumnName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DockerCompoisedDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeColumnName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Namw",
+                table: "Employees",
+                newName: "Name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Employees",
+                newName: "Namw");
+        }
+    }
+}
